67f41355330915d39f16e2201f3f71818d8d20cf
Mike Scherban <m-scherban@ti.com>
2016-08-29 15:27:48 -0500

    Show what the test.py outputs

--- test.py
+++ test.py
@@ -13,3 +13,19 @@ if evm1bmc.match('BOOT COMPLETE'):
     print('boot completed!')
 
 evm1bmc.close()
+
+#The output of this looks like:
+#
+#mike@ubuntu:~/workdir/pyserial/Evmmsger$ sudo ./test.py 
+#/dev/ttyUSB1: Hello World!
+#/dev/ttyUSB1: |   [00:00:24]  Executing command "reboot"
+#/dev/ttyUSB1: |   [00:00:24]  Full Reset Begin
+#/dev/ttyUSB1: |   [00:00:24]  Full Reset Complete
+#/dev/ttyUSB1: |   [00:00:24]  SOC RST Begin
+#/dev/ttyUSB1: |   [00:00:24]  Current BootMode is set to dsp no boot 
+#/dev/ttyUSB1: |   [00:00:24]  SOC RST Complete
+#/dev/ttyUSB1: |   [00:00:25]  BOOT COMPLETE    
+#/dev/ttyUSB1: |   BMC>
+#boot completed!
+#mike@ubuntu:~/workdir/pyserial/Evmmsger$ 
+
